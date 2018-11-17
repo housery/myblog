a6ae5018c0803b6a97319cf12581b3249bd790e9
xiaohoo <1126457667@qq.com>
2018-11-17 21:29:44 +0800

    添加dao扫描，增加测试

--- myblog-backend/src/main/java/com/xiaohoo/myblog/MyblogBackendApplication.java
+++ myblog-backend/src/main/java/com/xiaohoo/myblog/MyblogBackendApplication.java
@@ -2,8 +2,11 @@ package com.xiaohoo.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+// 扫描dao接口
+@MapperScan(basePackages = "com.xiaohoo.myblog.dao")
 public class MyblogBackendApplication {
 
     public static void main(String[] args) {
--- myblog-backend/src/test/java/com/xiaohoo/myblog/MyblogBackendApplicationTests.java
+++ myblog-backend/src/test/java/com/xiaohoo/myblog/MyblogBackendApplicationTests.java
@@ -1,16 +1,52 @@
 package com.xiaohoo.myblog;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.xiaohoo.myblog.dao.UserMapper;
+import com.xiaohoo.myblog.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
+@Rollback
 public class MyblogBackendApplicationTests {
+
+    private static Logger logger = LoggerFactory.getLogger(MyblogBackendApplicationTests.class);
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void userListTest() throws Exception{
+
+        PageHelper.startPage(0, 2);
+
+        // 设置分页查询条件
+        Example example = new Example(User.class);
+        PageInfo<User> pageInfo = new PageInfo<>(userMapper.selectByExample(example));
+
+        // 获取查询结果
+        List<User> userList = pageInfo.getList();
+        for (User user: userList){
+            logger.info("----->" + user.getName() + " " + user.getCreateDate());
+        }
     }
 
 }
